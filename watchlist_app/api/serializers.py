163d440cd4e7e7dc45716afed3ed536d1cb16453
abdullah <apple@apples-MacBook-Air.local>
2024-07-20 12:04:34 +0500

    Model serializer all fields show

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,48 +1,24 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError('Name is too short')
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validates=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        # all fields show
+        fields = "__all__"
+        # exclude field not show active
+        # exclude = ['active']
 
 
-    # for create and Post request 
-    def create(self, validated_data):
-        """
-        Create and return a new `Movie` instance, given the validated data.
-        """
-        return Movie.objects.create(**validated_data)
-    
-    # For Update or Put
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Movie` instance, given the validated data.
-        """
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    
-
-    # def validate_name(self, value):
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError("Name is to short")
-    #     else:
-    #         return value
-
-
-
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is to short")
+        else:
+            return value
 
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Name and Discription souhd be diffrent')
         else:
             return data
-
